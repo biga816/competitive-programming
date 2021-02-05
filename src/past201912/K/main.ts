84eb02d4043c34b6da50dcd01d3e3e9ad8fa078d
akihiro.tanaka <big.a.0816@gmail.com>
2021-02-05 23:33:13 +0900

    fix answer of K

--- src/past201912/K/main.ts
+++ src/past201912/K/main.ts
@@ -1,27 +1,68 @@
 import * as fs from 'fs';
 const [N, ...data] = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
-const bossList = data.splice(0, Number(N)).map(Number);
+const bossNoList = data.splice(0, Number(N)).map(Number);
 const [Q, ...inputs] = data;
 const pairList = inputs.map((pair) => pair.split(' ').map(Number));
+const memberNoListMap: Map<number, number[]> = new Map();
 
-const myBossListMap: Map<number, number[]> = new Map();
-bossList.forEach((boss, i) => {
-  const myBosses: number[] = [];
-  let currentBoss = boss;
-  while (currentBoss !== -1) {
-    myBosses.push(currentBoss);
-    currentBoss = bossList.find((_, j) => {
-      const employee = j + 1;
-      return employee === currentBoss;
-    });
+enum QueueType {
+  In = 'in',
+  Out = 'out',
+}
+
+interface Queue {
+  type: QueueType;
+  memberNo: number;
+}
+
+// create tree
+let rootNo: number;
+bossNoList.forEach((bossNo, i) => {
+  const memberNo = i + 1;
+  if (bossNo === -1) {
+    rootNo = memberNo;
+  } else {
+    const memberNoList = memberNoListMap.get(bossNo) || [];
+    memberNoList.push(memberNo);
+    memberNoListMap.set(bossNo, memberNoList);
   }
-
-  const employee = i + 1;
-  myBossListMap.set(employee, myBosses);
-  return myBosses;
 });
 
-pairList.forEach(([employee, boss]) => {
-  const res = myBossListMap.get(employee).includes(boss) ? 'Yes' : 'No';
-  console.log(res);
+let queueList: Queue[] = [
+  {
+    type: QueueType.In,
+    memberNo: rootNo,
+  },
+];
+let idx = 0;
+const dfsIndexObj: { [key: number]: { in: number; out?: number } } = {};
+
+// search tree from root (dfs)
+while (queueList.length > 0) {
+  const { type, memberNo: targetNo } = queueList.pop();
+  if (type === QueueType.In) {
+    // save route (the way there)
+    dfsIndexObj[targetNo] = { in: idx };
+
+    // add index to queue list for the way back
+    queueList.push({ type: QueueType.Out, memberNo: targetNo });
+
+    // add members to queue list for the way there
+    if (memberNoListMap.get(targetNo) && memberNoListMap.get(targetNo).length > 0) {
+      const memberQueueList = memberNoListMap.get(targetNo).map((memberNo) => ({ type: QueueType.In, memberNo }));
+      queueList.push(...memberQueueList);
+    }
+  } else {
+    // save route (the way back)
+    dfsIndexObj[targetNo].out = idx;
+  }
+  idx++;
+}
+
+pairList.forEach(([memberNo, bossNo]) => {
+  if (dfsIndexObj[bossNo].in <= dfsIndexObj[memberNo].in && dfsIndexObj[memberNo].in < dfsIndexObj[bossNo].out) {
+    console.log('Yes');
+  } else {
+    console.log('No');
+  }
 });
